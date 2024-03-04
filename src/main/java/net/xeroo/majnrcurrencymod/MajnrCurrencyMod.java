3edacafec0075454cbdf4587daac2d2077ff600e
xerooqq <petrklusacek657@gmail.com>
2024-03-04 23:50:37 +0100

    Getting back old load. 12 to 18.

--- src/main/java/net/xeroo/majnrcurrencymod/MajnrCurrencyMod.java
+++ src/main/java/net/xeroo/majnrcurrencymod/MajnrCurrencyMod.java
@@ -9,3 +9,10 @@ public class MajnrCurrencyMod implements ModInitializer {
 	public static final String MOD_ID = "majnrcurrencymod";
     public static final Logger LOGGER = LoggerFactory.getLogger("majnrcurrencymod");
 
+	@Override
+	public void onInitialize() {
+
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
